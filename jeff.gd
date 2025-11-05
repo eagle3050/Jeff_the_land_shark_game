c49416bea69095ff41d214bf48bca2c4a9c49ad2
eagle3050 <jake@clarke.biz>
2025-11-05 18:47:49 +1300

    Nearly added wall swim
    Few issues but set groundwork

--- jeff.gd
+++ jeff.gd
@@ -5,23 +5,40 @@ var speed = 300.0
 var jump_height = -500.0
 var can_shark_mode = true
 var in_shark_mode = false
+var on_wall = false
 
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		jump()
-	var direction := Input.get_axis("left", "right")
-	if direction:
-		velocity.x = direction * speed
-	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
-	move_and_slide()
+		
+	if not on_wall:
+		var direction := Input.get_axis("left", "right")
+
+		if direction:
+			velocity.x = direction * speed
+		else:
+			velocity.x = move_toward(velocity.x, 0, speed)
+		move_and_slide()
+
+	var direction_on_wall := Input.get_axis("up", "down")
+
+	if direction_on_wall:
+		velocity.y = direction_on_wall * speed
+		move_and_slide()
+
 	if Input.is_action_just_pressed("shark_mode") and can_shark_mode:
 		enter_shark_mode()
 	elif in_shark_mode and Input.is_action_just_pressed("shark_mode") or Input.is_action_just_pressed("jump") and not can_shark_mode:
 		exit_shark_mode()
+	elif is_on_wall() and in_shark_mode:
+		on_wall = true
+
+	elif not is_on_wall() and not in_shark_mode:
+		on_wall = false
 	
 func jump():
 	velocity.y = jump_height
